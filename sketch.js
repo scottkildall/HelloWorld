86fd0a5d5188092a7c5e767416d37f6fddf4b0c0
Scott Kildall <4422698+scottkildall@users.noreply.github.com>
2022-01-27 15:24:05 -0800

    first commit

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+// Setup code goes here
+function setup() {
+  createCanvas(1000, 800);
+
+  textSize(48);
+  textAlign(CENTER);
+ }
+
+
+// Draw code goes here
+function draw() {
+  background(0);
+
+  fill(255,0,100);
+  text('Hello, World', width/2, height/2);
+}
